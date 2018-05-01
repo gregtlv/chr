08c33297718b9c5e9e95ae6aece1c844c35e0f65
Greg Mac <manov@manov.ru>
2018-05-01 20:00:19 +0300

    Opana

--- js/script.js
+++ js/script.js
@@ -13,18 +13,17 @@ toggleButton.onclick = function menuHide() {
 
 
 	$(document).ready(function(){
-	 $("#headerNav").on("click","a", function (event) {
+	 $("#navList").on("click","a", function (event) {
 	   //отменяем стандартную обработку нажатия по ссылке
 	   event.preventDefault();
 
 	   //забираем идентификатор бока с атрибута href
 	   var id  = $(this).attr('href'),
+
 	   //узнаем высоту от начала страницы до блока на который ссылается якорь
-	     top = $(id).offset().top,
-			 link = document.getElementById("navList");
+	     top = $(id).offset().top;
 
 	   //анимируем переход на расстояние - top за 1500 мс
 	   $('body,html').animate({scrollTop: top}, 1500);
-		 link.style.display = "none";
 	 });
 	});
