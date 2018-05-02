ba58222797d6bf23d713735b81910e77a6be9b62
Greg Mac <manov@manov.ru>
2018-05-02 21:52:02 +0300

    Evening 02/05 #2

--- js/script.js
+++ js/script.js
@@ -28,5 +28,23 @@ toggleButton.onclick = function menuHide() {
 	 });
 	});
 
+	//Scrol toggleButton
+
+	$(document).ready(function(){
+	 $("#scrollButton").on("click","a", function (event) {
+		 //отменяем стандартную обработку нажатия по ссылке
+		 event.preventDefault();
+
+		 //забираем идентификатор бока с атрибута href
+		 var id  = $(this).attr('href'),
+
+		 //узнаем высоту от начала страницы до блока на который ссылается якорь
+			 top = $(id).offset().top;
+
+		 //анимируем переход на расстояние - top за 1500 мс
+		 $('body,html').animate({scrollTop: top}, 1500);
+	 });
+	});
+
 
 // noJs
